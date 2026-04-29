e92aef998771a861d2015a572fed60bf657a3a17
iSwaidy <99csantamaria@gmail.com>
2026-04-29 00:03:28 -0400

    Analysis Script Updated!

--- Ramirez_sales_analysis.sql
+++ Ramirez_sales_analysis.sql
@@ -6,25 +6,19 @@ Capstone 01           -- Territory: Maryland
 
 USE sample_sales;
 SHOW TABLES;
-DESCRIBE inventory_categories;
 
 -- Check Management table
 SELECT * FROM management
 WHERE salesmanager= 'Shruti Reddy';
 
--- What is total revenue overall for sales in the assigned territory, plus the start date and end date that tell you what period the data covers?
-
-SELECT * From store_sales;
-SELECT * From store_locations
-WHERE State = 'Maryland';
+-- 1. What is total revenue overall for sales in the assigned territory, plus the start date and end date that tell you what period the data covers?
 
 SELECT 
-		SUM(sale_amount) AS 'Total Revenue',
-        MIN(Transaction_Date) AS 'Start Date',
-        MAX(Transaction_Date) AS 'End Date'
+	MIN(Transaction_Date) AS 'Start Date',
+	MAX(Transaction_Date) AS 'End Date',
+	CONCAT('$', FORMAT(SUM(sale_amount), 2)) AS 'Total Revenue'
 FROM store_sales s
-JOIN store_locations sl 
-	ON s.store_id = sl.storeid
+JOIN store_locations sl ON s.store_id = sl.storeid
 WHERE State = 'Maryland';
 
 -- =======================================
@@ -34,7 +28,7 @@ WHERE State = 'Maryland';
 -- End Date: 2025-12-31
 -- =======================================
 
--- What is the month by month revenue breakdown for the sales territory?
+-- 2. What is the month by month revenue breakdown for the sales territory?
 
 SELECT
     YEAR(Transaction_Date) AS 'Year',
@@ -46,7 +40,7 @@ WHERE State = 'Maryland'
 GROUP BY YEAR(Transaction_Date), MONTH(Transaction_Date), MONTHNAME(Transaction_Date)
 ORDER BY YEAR(Transaction_Date), MONTH(Transaction_Date);
 
--- Provide a comparison of total revenue for the specific sales territory and the region it belongs to.
+-- 3. Provide a comparison of total revenue for the specific sales territory and the region it belongs to.
 
 SELECT 
 	CONCAT('$', FORMAT(SUM(CASE WHEN sl.state = 'Maryland' THEN s.sale_amount ELSE 0 END), 2)) AS 'Maryland Total Revenue',
@@ -61,13 +55,13 @@ JOIN management m ON sl.state = m.state;
 -- Northeast Total Revnue: $24,237526.98
 -- =======================================
 
--- What is the number of transactions per month and average transaction size by product category for the sales territory?
+-- 4. What is the number of transactions per month and average transaction size by product category for the sales territory?
 
 SELECT
     YEAR(Transaction_Date) AS 'Year',
     MONTHNAME(Transaction_Date) AS 'Month', ic.Category,
     COUNT(id) AS 'Number of Transactions',
-    AVG(Sale_Amount) AS 'Avg Transaction Size'
+    CONCAT('$', FORMAT(ROUND(AVG(Sale_Amount), 2), 2)) AS 'Average Transaction'
 FROM store_sales s
 JOIN store_locations sl ON s.store_id = sl.storeid
 JOIN products p ON s.prod_num = p.prodnum
@@ -76,9 +70,29 @@ WHERE sl.state = 'Maryland'
 GROUP BY YEAR(s.transaction_date), MONTH(s.transaction_date), MONTHNAME(s.transaction_date), ic.category
 ORDER BY YEAR(s.transaction_date), MONTH(s.transaction_date);
 
+-- 5. Can you provide a ranking of in-store sales performance by each store in the sales territory, or a ranking of online sales performance by state within an online sales territory?
 
--- Can you provide a ranking of in-store sales performance by each store in the sales territory, or a ranking of online sales performance by state within an online sales territory?
+SELECT
+    RANK() OVER (ORDER BY SUM(s.Sale_Amount) DESC) AS 'Rank',
+    sl.StoreLocation AS 'Store',
+    CONCAT('$', FORMAT(SUM(s.Sale_Amount),2)) AS 'Total Revenue'
+FROM store_sales s
+JOIN store_locations sl ON s.store_id = sl.storeid
+WHERE sl.State = 'Maryland'
+GROUP BY sl.StoreId, sl.StoreLocation
+ORDER BY SUM(s.Sale_Amount) DESC;
+    
+-- 6. What is your recommendation for where to focus sales attention in the next quarter?
 
--- What is your recommendation for where to focus sales attention in the next quarter?
+/*
+
+
+
+
+
+
+
+
+*/
 
 
